30ad3a9ae070f8ac4484ad38835e9c3657ade15b
Xucong Zhan <zhanxucong@gmail.com>
2021-04-07 23:27:44 -0400

    Added sorting capability; Applied workaround to Sqlite decimal type problem.

--- Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
+++ Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
@@ -6,10 +6,26 @@ namespace Core.Specifications
 {
     public class ProductsWithBrandsAndTypesSpecification : BaseSpecification<Product>
     {
-        public ProductsWithBrandsAndTypesSpecification()
+        public ProductsWithBrandsAndTypesSpecification(string sort)
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithBrandsAndTypesSpecification(int id) : base(x => x.Id == id)
