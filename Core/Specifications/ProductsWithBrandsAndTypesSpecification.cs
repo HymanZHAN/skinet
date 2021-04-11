74e770f30e512a24e71060b42c3f98aa218ca37d
Xucong Zhan <zhanxucong@gmail.com>
2021-04-10 22:10:28 -0400

    End of Section 06; Added pagination and filtering.

--- Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
+++ Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
@@ -6,14 +6,22 @@ namespace Core.Specifications
 {
     public class ProductsWithBrandsAndTypesSpecification : BaseSpecification<Product>
     {
-        public ProductsWithBrandsAndTypesSpecification(string sort)
+        public ProductsWithBrandsAndTypesSpecification(ProductSpecParams productParams)
+        : base(x =>
+           (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId)
+           && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+           && (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
+        )
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1),
+                        productParams.PageSize);
 
-            if (!string.IsNullOrEmpty(sort))
+            if (!string.IsNullOrEmpty(productParams.Sort))
             {
-                switch (sort)
+                switch (productParams.Sort)
                 {
                     case "priceAsc":
                         AddOrderBy(p => p.Price);
