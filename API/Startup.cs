67b3a3915e803e6bb8c14c823fff87ac9f9df552
Xucong Zhan <zhanxucong@gmail.com>
2021-05-09 23:06:24 -0400

    Added basket API; End of Secion 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -28,6 +29,12 @@ namespace API
             services.AddDbContext<StoreContext>(
                 x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection"))
             );
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_configuration.GetConnectionString("Redis"),
+                                                               true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddApplicationServices();
             services.AddCors(option =>
