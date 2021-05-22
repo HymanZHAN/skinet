876a578b7fdb494cd1fc51912b6418fbd883d9f0
Xucong Zhan <zhanxucong@gmail.com>
2021-05-21 22:25:02 -0400

    End of Section 16: Added validation to DTOs

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(
+            "(?=^.{6,}$)((?=.*\\w)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[|!\"$%&\\/\\(\\)\\?\\^\\'\\\\\\+\\-\\*]))^.*",
+            ErrorMessage = "Password must be of minimum length of 6 and has at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character"
+        )]
         public string Password { get; set; }
     }
 }
