fa4f53f1ec906cf8d017479171a75d4f438361d8
Xucong Zhan <zhanxucong@gmail.com>
2021-05-31 15:31:09 -0400

    End of Section 20: Completed checkout-success component

--- client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IOrder } from 'src/app/shared/models/order.model';
 
@@ -6,15 +6,20 @@ import { IOrder } from 'src/app/shared/models/order.model';
   selector: 'app-checkout-success',
   templateUrl: './checkout-success.component.html',
   styleUrls: ['./checkout-success.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckoutSuccessComponent {
   order: IOrder | undefined;
+  orderLink = '';
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation?.extras?.state;
     if (state) {
       this.order = state as IOrder;
+      this.orderLink = `/orders/${this.order.id}`;
+    } else {
+      this.orderLink = `/orders`;
     }
   }
 }
